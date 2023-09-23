a67921460f853bbca1757d9c901ffd90504b2a5c
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-23 14:58:27 -0600

    Initial setup

---
+++ basnet-segmentation/main.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+
+import keras_cv
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, backend
+
