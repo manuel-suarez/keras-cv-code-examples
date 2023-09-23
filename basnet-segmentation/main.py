b40bfc1a85aec1b402533d4db91be84f77949e89
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-23 15:00:18 -0600

    Hyperparameters

--- basnet-segmentation/main.py
+++ basnet-segmentation/main.py
@@ -8,3 +8,9 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, backend
 
+# Hyperparameters definition
+IMAGE_SIZE = 288
+BATCH_SIZE = 4
+OUT_CLASSES = 1
+TRAIN_SPLIT_RATIO = 0.90
+DATA_DIR = "/home/hasimoto/data/DUTS-TE"
