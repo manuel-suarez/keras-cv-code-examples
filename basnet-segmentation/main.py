e23e01b2b33244abfcdd2e5c07cea97f37ae2b61
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-23 19:59:29 -0600

    Dataset configuration

--- basnet-segmentation/main.py
+++ basnet-segmentation/main.py
@@ -14,3 +14,50 @@ BATCH_SIZE = 4
 OUT_CLASSES = 1
 TRAIN_SPLIT_RATIO = 0.90
 DATA_DIR = "/home/hasimoto/data/DUTS-TE"
+
+# Create dataset
+def load_paths(path, split_ratio):
+    images = sorted(glob(os.path.join(path, "DUTS-TE-Image/*")))[:140]
+    masks = sorted(glob(os.path.join(path, "DUTS-TE-Mask/*")))[:140]
+    len_ = int(len(images) * split_ratio)
+    return (images[:len_], masks[:len_]), (images[len_:], masks[len_:])
+
+def read_image(path, size, mode):
+    x = keras.utils.load_img(path, target_size=size, color_mode=mode)
+    x = keras.utils.img_to_array(x)
+    x = (x / 255.0).astype(np.float32)
+    return x
+
+def preprocess(x_batch, y_batch, img_size, out_classes):
+    def f(_x, _y):
+        _x, _y = _x.decode(), _y.decode()
+        _x = read_image(_x, (img_size, img_size), mode="rgb") # image
+        _y = read_image(_y, (img_size, img_size), mode="grayscale") # mask
+        return _x, _y
+
+    images, masks = tf.numpy_function(f, [x_batch, y_batch], [tf.float32, tf.float32])
+    images.set_shape([img_size, img_size, 3])
+    masks.set_shape([img_size, img_size, out_classes])
+    return images, masks
+
+def load_dataset(image_paths, mask_paths, img_size, out_classes, batch, shuffle=True):
+    dataset = tf.data.Dataset.from_tensor_slices((image_paths, mask_paths))
+    if shuffle:
+        dataset = dataset.cache().shuffle(buffer_size=1000)
+    dataset = dataset.map(
+        lambda x, y: preprocess(x, y, img_size, out_classes),
+        num_parallel_calls=tf.data.AUTOTUNE,
+    )
+    dataset = dataset.batch(batch)
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+    return dataset
+
+train_paths, val_paths = load_paths(DATA_DIR, TRAIN_SPLIT_RATIO)
+train_dataset = load_dataset(
+    train_paths[0], train_paths[1], IMAGE_SIZE, OUT_CLASSES, BATCH_SIZE, shuffle=True
+)
+val_dataset = load_dataset(
+    val_paths[0], val_paths[1], IMAGE_SIZE, OUT_CLASSES, BATCH_SIZE, shuffle=False
+)
+print(f"Train Dataset: {train_dataset}")
+print(f"Validation Dataset: {val_dataset}")
